3051201b1f217cf32d2b29ed444e918373e26ad4
Richard Levitte <richard@levitte.org>
2013-08-14 13:24:07 +0200

    * pf_safety/start.php: Handle groups and river in the same manner as   members.  Enhance the comments a bit.
    * pf_safety/manifest.xml: Add dependency on groups.  Add license
      information.  Bump version to 0.5.

--- pf_safety/start.php
+++ pf_safety/start.php
@@ -4,16 +4,39 @@
 register_elgg_event_handler('init','system','pf_safety_init',20000);
  
 function pf_safety_init() {
-  // Have the members page be protected with a gatekeeper() call
-  elgg_unregister_page_handler('members');
+  // Have the members page be removed from the site menu if the visitor isn't
+  // logged in, as well as protected with a gatekeeper() call
   if (!elgg_is_logged_in()) 
     elgg_unregister_menu_item('site', 'members');
+  elgg_unregister_page_handler('members');
   elgg_register_page_handler('members', 'pf_safety_members_page_handler');
+
+  // Have the groups page protected in a similar fashion.
+  if (!elgg_is_logged_in()) 
+    elgg_unregister_menu_item('site', 'groups');
+  elgg_unregister_page_handler('groups');
+  elgg_register_page_handler('groups', 'pf_safety_groups_page_handler');
+
+  // Have the groups page protected in a similar fashion.
+  if (!elgg_is_logged_in()) 
+    elgg_unregister_menu_item('site', 'activity');
+  elgg_unregister_page_handler('activity');
+  elgg_register_page_handler('activity', 'pf_safety_elgg_river_page_handler');
 }
 
 function pf_safety_members_page_handler($page) {
   gatekeeper();
   return call_user_func('members_page_handler', $page);
+}    
+
+function pf_safety_groups_page_handler($page) {
+  gatekeeper();
+  return call_user_func('groups_page_handler', $page);
+}    
+
+function pf_safety_elgg_river_page_handler($page) {
+  gatekeeper();
+  return call_user_func('elgg_river_page_handler', $page);
 }    
 
 ?>
