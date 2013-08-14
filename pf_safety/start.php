c6ea005789479c5395c8c0a73fb16b7b4e930b9c
Richard Levitte <richard@levitte.org>
2013-08-14 13:01:39 +0200

    * pf_safety/start.ph: In addition to having a gatekeeper() call in the   page handler, remove the menu item for this plugin from the site   menu bar if the visitor isn't logged in.

--- pf_safety/start.php
+++ pf_safety/start.php
@@ -6,6 +6,8 @@ register_elgg_event_handler('init','system','pf_safety_init',20000);
 function pf_safety_init() {
   // Have the members page be protected with a gatekeeper() call
   elgg_unregister_page_handler('members');
+  if (!elgg_is_logged_in()) 
+    elgg_unregister_menu_item('site', 'members');
   elgg_register_page_handler('members', 'pf_safety_members_page_handler');
 }
 
