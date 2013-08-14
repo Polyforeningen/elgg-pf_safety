485dd03dc67128c6dd5eab2886d8f769bfcf3926
Richard Levitte <richard@levitte.org>
2013-08-14 12:26:48 +0200

    * pf_safety/start.php (pf_safety_init): Correct function name.

--- pf_safety/start.php
+++ pf_safety/start.php
@@ -6,7 +6,7 @@ register_elgg_event_handler('init','system','pf_safety_init',20000);
 function pf_safety_init() {
   // Have the members page be protected with a gatekeeper() call
   elgg_unregister_page_handler('members');
-  elgg_register_page_handler('members', 'my_new_members_page_handler');
+  elgg_register_page_handler('members', 'pf_safety_members_page_handler');
 }
 
 function pf_safety_members_page_handler($page) {
